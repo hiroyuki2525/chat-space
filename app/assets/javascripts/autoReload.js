282c34651d00ea80d097b6017f27bee3ba06b3bc
hiroki0909 <hiroyukidaisuki2525@gmail.com>
2020-07-28 22:29:35 +0900

    デプロイ

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -40,27 +40,35 @@ $(function(){
     };
   }
 
-  $('.Form').on('submit', function(e){
-    e.preventDefault();
-    let formData = new FormData(this);
-    let url = $(this).attr('action');
+  let reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    let last_message_id = $('.MessageBox:last').data("message-id") || 0;
     $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
       dataType: 'json',
-      processData: false,
-      contentType: false
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
     })
-    .done(function(data){
-      let html = buildHTML(data);
-      $('.MessageField').append(html);      
-      $('form')[0].reset();
-      $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});
-      $('.Form__submit').prop("disabled", false);
+    .done(function(messages) {
+      // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        let insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.MessageField').append(insertHTML);
+        $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});
+      }
     })
     .fail(function() {
-      alert("メッセージ送信に失敗しました");
-      $('.Form__submit').prop("disabled", false);
+      alert('error');
     });
-  });
+  };
+  setInterval(reloadMessages, 7000);
+});
